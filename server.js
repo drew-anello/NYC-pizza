54fa5de73fe89dee569b785ce827c1d0c9c80ca3
Drew Anello <drew.anello@gmail.com>
2022-09-12 14:40:25 -0400

    corupted file fix

---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
+
+
+
+
+// Index
+
+// New
+
+// Delete
+
+// Update
+
+// Create
+
+// Edit
+
+// Show
+
+// listener
+app.listen(PORT, (req, res) => {
+console.log('happy birthday mr president')   
+})
+
