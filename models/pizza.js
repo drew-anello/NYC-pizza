d8e82c298de656906340511524421ca88b6b6a3e
Drew Anello <drew.anello@gmail.com>
2022-09-13 13:20:36 -0400

    wip: seed is showing on mongoDB

--- models/pizza.js
+++ models/pizza.js
@@ -1,17 +1,13 @@
 const { default: mongoose } = require("mongoose")
 
 const Schema = mongoose.Schema
-const pizzaSchema = new Schema({
-    name: String
-}, {
-    adress: String
-}, {
-    borough: String
-}, {
-    review: [String]
-}, {
-    rating: Number
-}
-)
+const pizzaSchema = new Schema ({
+	name: { type: String, required: true },
+	adress: { type: String, required: true },
+	borough: { type: String, required: true },
+	review: { type: [String], required: true },
+	score: { type: Number, min: 0.1, max: 10.0, required: true },
+
+})
 const pizzaTime = mongoose.model('Pizza', pizzaSchema)
 module.exports = pizzaTime
